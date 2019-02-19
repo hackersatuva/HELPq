7652404d58eae638e70390cd8ef4cb8f43fdcc82
Rithik Yelisetty <rithik@gmail.com>
2019-02-18 22:14:42 -0500

    branding changes

---
+++ .meteor/heroku_build/app/programs/server/app/server/lib/permissions.js
@@ -0,0 +1,22 @@
+(function(){// ----------------------------------------
+// Basic Roles, Client side auth
+// ----------------------------------------
+
+// Admin have all rights
+authorized = {
+  user: function(id){
+    var user = _getUser(id);
+    return user ? true : false;
+  },
+  admin: function(id){
+    var user = _getUser(id);
+    return user.profile.admin
+  },
+  mentor: function(id){
+    var user = _getUser(id);
+    return user.profile.admin || user.profile.mentor
+  }
+};
+}).call(this);
+
+//# sourceMappingURL=permissions.js.map
